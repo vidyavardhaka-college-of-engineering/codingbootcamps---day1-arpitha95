ef77737065557ec35f0b1f0ca1fbd506c47da9b9
Arpitha_Bharadhwaj <>
2021-05-24 12:09:24 +0000

    printf statement

--- Program_2.c
+++ Program_2.c
@@ -1,4 +1,4 @@
-//The given C program doesn&#39;t work as expected. You need to correct the below program and make it executable.
+//The given C program doesn't work as expected. You need to correct the below program and make it executable.
 //(Note: It should able to read an integer from the user and print it)
 
 //Example
@@ -12,6 +12,7 @@ int main()
 {
 //Decleade Integer n and write scanf code bellow 
 
+printf("Enter an Interger Value\n");
 
 //if you write correcr code your value will print
 printf("%d",n);
